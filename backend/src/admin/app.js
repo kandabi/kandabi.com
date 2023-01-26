ef179a57324887fdb148de1d1199416359a49f8c
Aviv Kandabi <kandabiaviv@gmail.com>
2023-01-26 17:04:33 +0200

    Update into monorepo

---
+++ backend/src/admin/app.js
@@ -0,0 +1,12 @@
+const config = {
+  locales: [],
+};
+
+const bootstrap = (app) => {
+  console.log(app);
+};
+
+export default {
+  config,
+  bootstrap,
+};
--- config/plugins.js
+++
@@ -1,5 +0,0 @@
-module.exports = ({ env }) => ({
-  comments: {
-    enabled: true,
-  }
-});
