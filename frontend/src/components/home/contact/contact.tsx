ce66dba814c293284f54c2728f44ed4c25b30077
Aviv Kandabi <kandabiaviv@gmail.com>
2023-02-07 13:13:09 +0200

    Update contact.tsx

--- frontend/src/components/home/contact/contact.tsx
+++ frontend/src/components/home/contact/contact.tsx
@@ -1,16 +1,12 @@
 import styled from 'styled-components';
 
 import { ParallaxScroll } from 'components/common/parallax';
-import { Footer } from 'components/common/footer';
-
 const ContactStyled = styled.div`
    background: linear-gradient(13deg, rgba(131, 58, 180, 1) 0%, rgba(29, 116, 253, 1) 50%, rgba(252, 176, 69, 1) 100%);
    justify-content: center;
    align-items: center;
    position: relative;
    display: flex;
-
-   /* position: -webkit-sticky; */
    height: 100%;
    width: 100%;
 `;
@@ -22,10 +18,9 @@ const TitleStyled = styled.h1`
 
 const Contact = () => {
    return (
-      <ParallaxScroll distanceToCamera={2} height='100%' scale={1.5}>
+      <ParallaxScroll distanceToCamera={6} height='100%' scale={2}>
          <ContactStyled>
             <TitleStyled>Hello!!!!!!!!!!</TitleStyled>
-            {/* <Footer /> */}
          </ContactStyled>
       </ParallaxScroll>
    );
