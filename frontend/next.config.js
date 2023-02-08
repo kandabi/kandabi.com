8f518139bb50abcbe5bdc56d996a6ab6ea304113
Aviv Kandabi <kandabiaviv@gmail.com>
2023-02-08 18:36:55 +0200

    Remove next-export-optimize-images

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 
-const withExportImages = require('next-export-optimize-images')
+// const withExportImages = require('next-export-optimize-images');
 
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
   compiler: {
     styledComponents: true,
-  }
-}
+  },
+};
 
-module.exports = withExportImages(nextConfig);
+module.exports = nextConfig;
