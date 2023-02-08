d4ea6267726aed1286d67262a30dc53ec3a7d02e
Aviv Kandabi <kandabiaviv@gmail.com>
2023-02-08 20:50:45 +0200

    iOS wave fix

--- frontend/src/hooks/useIsMobile.tsx
+++ frontend/src/hooks/useIsMobile.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
-import MobileDetect from 'is-mobile';
+import { isMobile as isMobileDevice, isMobileSafari } from 'react-device-detect';
 
 const useIsMobile = () => {
    const [isMobile, setIsMobile] = useState<boolean>(false);
-   useEffect(() => setIsMobile(MobileDetect()), []);
+   useEffect(() => setIsMobile(isMobileSafari || isMobileDevice), []);
 
    return {
       isMobile,
