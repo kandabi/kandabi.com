c0ab611070ff7851ecf0d5295bbc8c9f36a61982
Aviv Kandabi <kandabiaviv@gmail.com>
2023-02-08 21:32:42 +0200

    Keep testing fuck apple

--- frontend/src/hooks/useIsMobile.tsx
+++ frontend/src/hooks/useIsMobile.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
-import { isMobile as isMobileDevice, isMobileSafari } from 'react-device-detect';
+import { isIOS, isMobile as isMobileDevice, isMobileSafari } from 'react-device-detect';
 
 const useIsMobile = () => {
    const [isMobile, setIsMobile] = useState<boolean>(false);
-   useEffect(() => setIsMobile(isMobileSafari || isMobileDevice), []);
+   useEffect(() => setIsMobile(isIOS || isMobileSafari || isMobileDevice), []);
 
    return {
       isMobile,
