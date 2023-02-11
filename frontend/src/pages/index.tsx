c325c37df36c13d4d60a1cee693d186fc3381d82
kandabi <aviv@kandabi.com>
2023-02-11 20:28:02 +0200

    Update index.tsx

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,21 +2,21 @@ import { GetStaticProps } from 'next';
 
 import { ProjectsApi } from 'api/projects';
 import { HomePage } from 'components/home-page';
-import { IProjectsContainer } from 'components/home-page/projects-about-section/projects-container';
 import { IProjectItem } from 'components/home-page/projects-about-section/project-item';
+import { IProjectsContainer } from 'components/home-page/projects-about-section/projects-container';
 
 const Index = ({ projects }: IProjectsContainer) => {
    return <HomePage projects={projects} />;
 };
 
 const getStaticProps: GetStaticProps<IProjectsContainer> = async (context) => {
-   // const jwtToken = process.env.JWT_API_TOKEN!;
-   // if (!jwtToken) {
-   //    console.error('Missing JWT_API_TOKEN, have you added it to environment variables??');
-   // }
-
-   // const projects = await ProjectsApi.get(jwtToken);
-   const projects: IProjectItem[] = [];
+   let projects: IProjectItem[] = [];
+   const jwtToken = process.env.JWT_API_TOKEN!;
+   if (!jwtToken) {
+      console.error('Missing JWT_API_TOKEN, have you added it to environment variables??');
+   } else {
+      projects = await ProjectsApi.get(jwtToken);
+   }
 
    return {
       props: { projects },
