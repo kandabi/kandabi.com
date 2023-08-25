db588c4c624470a3e51def51306e71f8c99c9d70
kandabi <kandabiaviv@gmail.com>
2023-08-25 20:04:49 +0300

    Save

---
+++ frontend/src/components/common/Navbar/NavbarItem.tsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+import { Line } from 'components/common/Line';
+import { Link } from 'components/common/Link';
+
+export enum NavbarItemVariants {
+    Link = 'Link',
+    Scroll = 'Scroll',
+}
+
+const NavbarItemStyled = styled.li`
+    position: relative;
+    font-size: 18px;
+    cursor: pointer;
+`;
+
+export interface NavbarItemProps {
+    lineStyles?: { bottom: string };
+    variant?: NavbarItemVariants;
+    onClick?: () => void;
+    title: string;
+    link?: string;
+}
+
+export const NavbarItem = ({
+    title,
+    link,
+    onClick,
+    lineStyles,
+    variant = NavbarItemVariants.Scroll,
+}: NavbarItemProps) => {
+    return (
+        <NavbarItemStyled>
+            {variant === NavbarItemVariants.Link ? (
+                <Link styles={{ lineBottom: lineStyles?.bottom }} text={title} href={link!} />
+            ) : (
+                <Line styles={{ bottom: lineStyles?.bottom }}>
+                    <span onClick={onClick}>{title}</span>
+                </Line>
+            )}
+        </NavbarItemStyled>
+    );
+};
