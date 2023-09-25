d2c8ccfc06fdb7ba62e954cc0a155d05aec22df5
Aviv Kandabi <kandabiaviv@gmail.com>
2023-09-25 10:09:34 +0300

    Save

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,9 +1,16 @@
+import path from 'path';
+
 export default {
     graphql: {
         config: {
             endpoint: '/graphql',
             shadowCRUD: true,
             playgroundAlways: false,
+            generateArtifacts: true,
+            artifacts: {
+                schema: path.join(__dirname, '..', 'schema.graphql'),
+                typegen: path.join(__dirname, '..', 'types.d.ts'),
+            },
             depthLimit: 7,
             amountLimit: 100,
             apolloServer: {
--- backend/dist/config/plugins.js
+++ backend/dist/config/plugins.js
@@ -1,11 +1,20 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const path_1 = __importDefault(require("path"));
 exports.default = {
     graphql: {
         config: {
             endpoint: '/graphql',
             shadowCRUD: true,
             playgroundAlways: false,
+            generateArtifacts: true,
+            artifacts: {
+                schema: path_1.default.join(__dirname, '..', 'schema.graphql'),
+                typegen: path_1.default.join(__dirname, '..', 'types.d.ts'),
+            },
             depthLimit: 7,
             amountLimit: 100,
             apolloServer: {
